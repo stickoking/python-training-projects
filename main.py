84a91685ae78c6f3166811395c7912a4e85d049a
Prasshant Shanmugalingam <stickoking@hotmail.com>
2022-07-10 00:41:36 +0800

    Initial Commit

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
-numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-# 🚨 Do Not Change the code above 👆
+import random
+import pandas
 
-#Write your 1 line code 👇 below:
+name = ['Alex', 'Bet', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
 
-squared_numbers = [x*x for x in numbers];
+students_scores = {students: random.randint(1, 100) for students in name}
 
-#Write your code 👆 above:
+print(students_scores)
 
-print(squared_numbers)
+passed_students = {passed: score for(passed, score) in students_scores.items() if score >= 60}
 
+print(passed_students)
+
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+
+word_char_count = {word: len(word) for word in sentence.split()}
+
+print(word_char_count)
+
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+
+weather_f = {day: round((temperature_c * (9/5) + 32), 2) for (day, temperature_c) in weather_c.items()}
+
+print(weather_f)
+
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+student_data_frame = pandas.DataFrame(student_dict)
+
+print(student_data_frame)
+
+#Looping through data frame
+for(index, row) in student_data_frame.iterrows():
+    print(row.student)
